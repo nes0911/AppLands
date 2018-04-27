4be1185a688755d7de68ec5d7d15c6f7428e4cb9
Nestor <nes0911@users.noreply.github.com>
2018-04-26 22:01:45 -0400

    Part 018 (Other Pages in Tab)

--- AppLands/AppLands/Models/Translations.cs
+++ AppLands/AppLands/Models/Translations.cs
@@ -32,6 +32,6 @@
         public string Croatian { get; set; }
 
         [JsonProperty(PropertyName = "fa")]
-        public string Danish { get; set; }
+        public string Persian { get; set; }
     }
 }
--- AppLands/AppLands/ViewModels/LandViewModel.cs
+++ AppLands/AppLands/ViewModels/LandViewModel.cs
@@ -9,6 +9,8 @@
     {
         #region Atributos
         private ObservableCollection<Border> borders;
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
         #endregion
 
         #region Propiedades
@@ -19,6 +21,18 @@
             get { return this.borders; }
             set { this.SetValue(ref this.borders, value); }
         }
+
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { this.SetValue(ref this.currencies, value); }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get { return this.languages; }
+            set { this.SetValue(ref this.languages, value); }
+        }
         #endregion
 
         #region Constructores
@@ -26,6 +40,8 @@
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
         #endregion
 
